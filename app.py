57dad77492172ad343c4e4fd46f9c60640aabd77
Fernando Leite <65674165+fernandodgl@users.noreply.github.com>
2022-02-07 19:48:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 #carregando as bibliotecas
 #import pandas as pd
 #import streamlit as st
-#from minio import Minio
-#import joblib
-#import matplotlib.pyplot as plt
-#from pycaret.classification import load_model, predict_model
+from minio import Minio
+import joblib
+import matplotlib.pyplot as plt
+from pycaret.classification import load_model, predict_model
 
 #Baixando os arquivos do Data Lake
 client = Minio(
